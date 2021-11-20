5c585dafd5e854264759dd0cf5499b3de4d98c14
makun <etukobamasatyan@gmail.com>
2021-11-20 16:21:24 +0900

    終了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-</template>
+  <div>
+    <input type = "text" v-model="mate">
+    <button @click="add_material()">追加</button>
+  </div>
+  <div>
+  <tr v-for="item in this.material.data" v-bind:key="item">
+      <th>{{item}}</th>
+  </tr>
+  </div>
+  <div>
+    <button @click="get_recipe()">検索</button>
+  </div>
+  
 
+</template>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data: () =>({
+      material: {
+        "data":[]
+      },
+      mate:"",
+      title:[],
+      getdata:""
+  }),
+  mounted(){
+
+  },
+  methods: {
+    get_recipe(){
+
+
+      axios.post('https://banmeshi.azurewebsites.net/get_db_recipe_one',this.material.data)
+      .then(function (response) {
+          console.log(response)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+    },
+    add_material(){
+        this.material.data.push(this.mate)
+        this.mate = ""
+    }
   }
 }
 </script>
