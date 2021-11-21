4d15f13951edaee47687a915ed619364c0d6568b
makun <etukobamasatyan@gmail.com>
2021-11-21 15:55:13 +0900

    ike

---
+++ dist/precache-manifest.c7e8fc7d6475e4105f6a661eb18ca25c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4359ff13b1923b77fe2",
+    "url": "/css/app.99180e47.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "819de62083da704a2a7c4ba6a288b9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84dfa13d332db25e5de6",
+    "url": "/js/about.5421c6d7.js"
+  },
+  {
+    "revision": "f4359ff13b1923b77fe2",
+    "url": "/js/app.719df190.js"
+  },
+  {
+    "revision": "8652d210b903a1050417",
+    "url": "/js/chunk-vendors.5d5a8dd7.js"
+  },
+  {
+    "revision": "8e299c6dba7be4ae908ede0fdb8da3bd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
